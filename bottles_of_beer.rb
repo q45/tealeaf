cdcdfc0ee92434830ce59ae671f7e5a9bfc68b88
q45 <quintinsmith@icloud.com>
2013-07-26 11:42:34 -0600

    modified bottles of beer on the wall

--- bottles_of_beer.rb
+++ bottles_of_beer.rb
@@ -15,7 +15,7 @@ while bottles > 0
 	puts("#{bottles} bottles of beer".center(line_width))
 	take_one_down
 	bottles = bottles - 1.to_i
-	puts((bottles.to_s + (bottles == 1 ? " bottle" : (bottles == 0 ? "  bottles left, time to go to the store" : "bottles")) + " of beer on the wall").center(line_width))
+	puts((bottles.to_s + (bottles == 1 ? " bottle" : (bottles == 0 ? "  bottles left, time to go to the store" : "bottles of beer on the wall"))).center(line_width))
 end
 
 
