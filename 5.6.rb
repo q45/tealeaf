3e472ce1e72fcb2c89f48f86bf6e0a4585b92b88
q45 <quintinsmith@icloud.com>
2013-07-26 09:23:32 -0600

    5_6 excercise

---
+++ 5.6.rb
@@ -0,0 +1,11 @@
+puts "Please enter your first name"
+first_name = gets.chomp
+
+puts "Enter your middle name"
+middle_name = gets.chomp
+
+puts "Enter your last name"
+last_name = gets.chomp
+
+puts "Hello #{first_name} #{middle_name} #{last_name} it is a pleasure to meet you"
+
