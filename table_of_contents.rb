969a9837d2c56fda110d056198212d083f8f0122
q45 <quintinsmith@icloud.com>
2013-07-26 10:13:10 -0600

    table of contents excercise

---
+++ table_of_contents.rb
@@ -0,0 +1,9 @@
+line_width = 70 
+
+puts('Table of Contents'.center(line_width))
+puts ""
+puts('Chapter 1: Getting Started'.ljust(line_width/2.to_i) +  'page 1'.rjust(line_width/2.to_i))
+puts('Chapter 2: Numbers'.ljust(line_width/2.to_i) +          'page 9'.rjust(line_width/2.to_i))
+puts('Chapter 3: Letters'.ljust(line_width/2.to_i) +          'page 13'.rjust(line_width/2.to_i))
+
+
